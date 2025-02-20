1864e096c48e01aef4859d101f40566558331183
giorgizena123 <zenaishvili621@gmail.com>
2025-02-20 23:35:39 +0400

    Update Country.java

--- src/Country.java
+++ src/Country.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.Scanner;
 
-public class Country {
+class Country implements Serializable {
+    String name;
+    int population;
+    double area;
+
+    public Country(String name, int population, double area) {
+        this.name = name;
+        this.population = population;
+        this.area = area;
+    }
+
+    @Override
+    public String toString() {
+        return "ქვეყანა: " + name + ", მოსახლეობა: " + population + ", ფართობი: " + area;
+    }
 }
