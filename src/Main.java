70ca9fdc95274567db17b017ab183438f648e413
giorgizena123 <zenaishvili621@gmail.com>
2025-02-20 23:36:08 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Country[] countries = new Country[5];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("შეიყვანეთ ინფორმაცია " + (i + 1) + " ქვეყნის შესახებ:");
+            System.out.print("დასახელება: ");
+            String name = scanner.nextLine();
+            System.out.print("მოსახლეობა: ");
+            int population = scanner.nextInt();
+            System.out.print("ფართობი: ");
+            double area = scanner.nextDouble();
+            scanner.nextLine();
+            countries[i] = new Country(name, population, area);
+        }
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("countries.txt"))) {
+            oos.writeObject(countries);
+            System.out.println("ინფორმაცია წარმატებით ჩაიწერა ფაილში.");
+        } catch (IOException e) {
+            System.err.println("შეცდომა ფაილში ჩაწერისას: " + e.getMessage());
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("countries.txt"))) {
+            Country[] readCountries = (Country[]) ois.readObject();
+            System.out.println("\nწაკითხული ინფორმაცია ქვეყნების შესახებ:");
+            for (Country country : readCountries) {
+                System.out.println(country);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("შეცდომა ფაილიდან წაკითხვისას: " + e.getMessage());
         }
     }
 }
